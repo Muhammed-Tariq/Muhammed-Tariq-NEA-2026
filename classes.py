8a0d0f10e51a4742a7ac7de451a5ca198a22bacb
Muhammed Tariq <seriauora@gmail.com>
2025-11-12 20:50:46 +0000

    Button class
    Also updates when hovering!

--- classes.py
+++ classes.py
@@ -1,1 +1,28 @@
+import pygame as py
 
+class Button(): 
+    def __init__(self, position, filename, text, font, colour, hoverColour): 
+        self.image = py.image.load(filename)
+        self.position = position
+        self.rect = self.image.get_rect(center = position)
+        self.height = self.image.get_height()
+        self.width = self.image.get_width()
+        self.text = text
+        self.font = font
+        self.colour = colour
+        self.hoverColour = hoverColour
+        self.tempColour = self.colour
+
+    def draw(self, screen):
+        titleText = self.font.render(self.text, True, self.colour) 
+        titleRect = titleText.get_rect(center = self.position)
+        screen.blit(self.image, self.rect)
+        screen.blit(titleText, titleRect)
+
+    def hover(self, position):
+        if self.rect.collidepoint(position):
+            self.colour = self.hoverColour
+            return True
+        else:
+            self.colour = self.tempColour
+            return False
--- menus.py
+++ menus.py
@@ -1,36 +1,61 @@
 import pygame as py
+from classes import Button
+
+# Constants
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
 
 # Pygame display initialisation
 
 py.display.init()
 py.font.init()
-screen = py.display.set_mode((1920, 1080), 0, 32)
+
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
+
 py.display.set_icon(py.image.load("assets/gameIcon.png"))
 py.display.set_caption("Mockfish")
-title = py.font.Font("assets/fonts/RedditSans-Bold.ttf", 32)
-buttonText = py.font.Font("assets/fonts/RedditSans-Medium.ttf", 16)
 
-# Helper functions
-
-def writeText(text, font, xy, colour = (255, 255, 255)):
-    screen.blit(font.render(text, True, colour), xy)
+TITLE = py.font.Font("assets/fonts/RedditSans-Bold.ttf", 150)
+BUTTON_TEXT = py.font.Font("assets/fonts/RedditSans-SemiBold.ttf", 55)
+BUTTON_IMAGE = py.image.load("assets/buttons/button.png")
+OPTIONS_BUTTON_IMAGE = py.image.load("assets/buttons/optionsButton.png")
 
 # Game loops
 
 def mainMenu():
-    """The main menu's game loop, which always runs when this program is executed."""
+    # The main menu's game loop, which always runs when this program is executed
 
     bg = py.image.load("assets/bg.png")
     screen.blit(bg, (0, 0))
 
-    writeText("Hello", title, (0, 0), (255, 255, 255))
+    titleText = TITLE.render("Mockfish", True, "#FFFFFF") 
+    titleRect = titleText.get_rect(center = (960, 160))
+    screen.blit(titleText, titleRect)
+
+    playButton = Button((960, 400), "assets/buttons/button.png", "Start Game", BUTTON_TEXT, "#FFFFFF", "#9C9C9C")
 
     playing = True
     while playing:
         py.display.flip()
+        playButton.hover(py.mouse.get_pos())
+        playButton.draw(screen)
         for event in py.event.get():
             if event.type == py.QUIT:
                 playing = False
                 py.quit()
+            if event.type == py.MOUSEBUTTONDOWN:
+                pass
+
+def initialiseGame():
+    pass
+
+def analyseGame():
+    pass
+
+def options():
+    pass
+
+
 
 mainMenu()
