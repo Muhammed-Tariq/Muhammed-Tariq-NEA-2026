05f3ba68fe4190c74dcff8e5442ae2b22e50a5a8
Muhammed Tariq <seriauora@gmail.com>
2025-11-18 21:10:59 +0000

    Generation of legal moves (without piece generators yet)

--- proj/gameLogic.py
+++ proj/gameLogic.py
@@ -11,6 +11,7 @@ class Board():
                       ["wP"] * 8,
                       ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
         self.moveHistory = []
+        self.legalMoves = []
         self.whiteToMove = True # True if White is to move, False if Black is to move
         self.position = position
         self.length = length
@@ -39,6 +40,28 @@ class Board():
             x = 0 + self.position[0]
             y += self.squareSize
 
+    def generateLegalMoves(self):
+        self.legalMoves = []
+        for r in range(8):
+            for c in range(8):
+                if self.board[r][c] == "":
+                    continue
+                if (self.whiteToMove and self.board[r][c][0] == "w") or (not self.whiteToMove and self.board[r][c][0] == "b"):
+                    if self.board[r][c][1] == "P":
+                        self.legalMoves.extend(self.pawnMoves(r, c))
+                    elif self.board[r][c][1] == "R":
+                        self.legalMoves.extend(self.rookMoves(r, c))
+                    elif self.board[r][c][1] == "N":
+                        self.legalMoves.extend(self.knightMoves(r, c))
+                    elif self.board[r][c][1] == "B":
+                        self.legalMoves.extend(self.bishopMoves(r, c))
+                    elif self.board[r][c][1] == "Q":
+                        self.legalMoves.extend(self.queenMoves(r, c))
+                    elif self.board[r][c][1] == "K":
+                        self.legalMoves.extend(self.kingMoves(r, c))
+                    
+
+
     def hoverSquare(self, mousePos):
         x = mousePos[0]
         y = mousePos[1]
@@ -54,10 +77,13 @@ class Board():
             r2, c2 = pos2
             piece1 = self.board[r1][c1]
             if piece1 != "":
-                print(self.indextoACN(pos2, piece1))
-                self.moveHistory.append(self.indextoACN(pos2, piece1))
-                self.board[r2][c2] = piece1
-                self.board[r1][c1] = ""
+                if (piece1[0] == "b" and self.whiteToMove == False) or (piece1[0] == "w" and self.whiteToMove == True):
+                    if [pos1, pos2] in self.legalMoves:
+                        print(self.indextoACN(pos2, piece1))
+                        self.moveHistory.append(self.indextoACN(pos2, piece1))
+                        self.whiteToMove = not self.whiteToMove
+                        self.board[r2][c2] = piece1
+                        self.board[r1][c1] = ""
 
     def indextoACN(self, pos2, piece):
         newPos = [pos2[0] + 1, pos2[1] + 1]
@@ -68,3 +94,25 @@ class Board():
         else:
             acn = str(col) + str(row)
         return acn
+    
+    # Piece movements
+
+    def pawnMoves(self, r, c):
+        pass
+
+    def bishopMoves(self, r, c):
+        pass
+
+    def knightMoves(self, r, c):
+        pass
+
+    def rookMoves(self, r, c):
+        pass
+
+    def queenMoves(self, r, c):
+        l1 = self.bishopMovements(self, r, c)
+        l2 = self.rookMovements(self, r, c)
+        return l1 + l2
+
+    def kingMoves(self, r, c):
+        pass
